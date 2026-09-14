c9c8a6d6d030e167d482633e566e015234e8f40a
Imron jehleh <imron.jehleh@gmail.com>
2026-09-15 01:40:39 +0700

    Try to fix blanking after predestrian sequence is finished. Not work...

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,59 @@
 #include "pin.h"
 #include "control.h"
 
+// PORTD 0b0000x0xx
+//       7: ^ Pedestrian: Countdown: Red
+//        6: ^ Pedestrian: Countdown: Green
+//         5: ^ Pedestrian: Light: Red
+//          4: ^ Pedestrian: Light: Green
+//           3: ^ UNUSED
+//            2: ^ Trigger
+
+// PORTB 0bxx000000
+//       13: ^ Car: Countdown: Red
+//        12: ^ Car: Countdown: Orange
+//         11: ^ Car: Countdown: Green
+//          10: ^ Car: Light: Red
+//            9: ^ Car: Light: Orange
+//             8: ^ Car: Light: Green
+
+// AND operation
+// a  b  a&b
+// 0  0   0
+// 0  1   0
+// 1  0   0
+// 1  1   1
+// x  0   0
+// x  1   x
+// x  x   x
+
+// OR operation
+// a  b  a|b
+// 0  0   0
+// 0  1   1
+// 1  0   1
+// 1  1   1
+// x  0   x
+// x  1   1
+// x  x   x
+
+// XOR operation
+// a  b  a|b
+// 0  0   0
+// 0  1   1
+// 1  0   1
+// 1  1   0
+// x  0   x
+// x  1  ~x
+// x  x   0
+
+// NOT operation
+// a  ~a
+// 0   1
+// 1   0
+// x  ~x
+
+
 unsigned long timeout = 0;
 
 void setup() {
@@ -23,10 +76,8 @@ void setup() {
     pinMode(carCountdownPin_ORANGE, OUTPUT);
     pinMode(carCountdownPin_RED, OUTPUT);
 
-    changeCarLightSignal(green);
-    changePedestrianLightSignal(red);
-    changeCarCountdownSignal(green, hide);
-    changePedestrianCountdownSignal(red, hide);
+    PORTD = 0b00100111;  // Pedestrian: Display: off     (00), Light: Red     (10)
+    PORTB = 0b11000001;  // Car:        Display: off    (000), Light: Green  (001)
 
     DEBUG_INFO("Ready");
     detectSignal(begSignalPin, 30);
@@ -35,29 +86,25 @@ void setup() {
 }
 
 void loop() {
-    changeCarCountdownSignal(green, show);
-    changePedestrianCountdownSignal(red, show);
+    PORTD = 0b10100111;  // Pedestrian: Display: RED     (10), Light: Red     (10)
+    PORTB = 0b11001001;  // Car:        Display: GREEN  (001), Light: Green  (001)
 
     delay(20 * 1000 + 50);
-    changeCarLightSignal(orange);
-    changeCarCountdownSignal(orange, show);
+    PORTB = 0b11010010;  // Car:        Display: ORANGE (010), Light: Orange (010)
+
 
     delay(3 * 1000 + 50);
-    changeCarLightSignal(red);
-    changeCarCountdownSignal(red, show);
-
+    PORTB = 0b11100100;  // Car:        Display: RED    (100), Light: Red    (100)
+    
     delay(2 * 1000 + 50);
-    changePedestrianLightSignal(green);
-    changePedestrianCountdownSignal(green, show);
+    PORTD = 0b01010111;  // Pedestrian: Display: GREEN   (01), Light: Green   (01)
 
     delay(15 * 1000 + 50);
-    changePedestrianLightSignal(red);
-    changePedestrianCountdownSignal(red, hide);
+    PORTD = 0b00100111;  // Pedestrian: Display: off     (00), Light: Red     (10)
 
     delay(2 * 1000 + 50);
-    changeCarLightSignal(green);
-    changeCarCountdownSignal(green, hide);
-
+    PORTB = 0b11000001;  // Car:        Display: off    (000), Light: Green  (001)
+    
     timeout = millis() + 60ul * 1000;
     DEBUG_INFO("Ready");
     detectSignal(begSignalPin, 30);
