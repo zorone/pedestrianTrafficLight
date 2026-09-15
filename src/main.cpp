88006848eec5126442213727569244caf183176b
Imron jehleh <imron.jehleh@gmail.com>
2026-09-15 16:35:12 +0700

    Try to cherry-pick codes from WIP_fallback_debug.

--- src/main.cpp
+++ src/main.cpp
@@ -60,6 +60,11 @@
 
 unsigned long timeout = 0;
 
+char buf_b[11], buf_d[11];
+volatile uint8_t PORTB_PRE, PORTB_POST, PORTD_PRE, PORTD_POST;
+
+char* binaryToStr(volatile uint8_t value, char* buffer);
+
 void setup() {
     Serial.begin(9600);
     Debug.timestampOn();
@@ -75,9 +80,12 @@ void setup() {
     pinMode(carCountdownPin_GREEN, OUTPUT);
     pinMode(carCountdownPin_ORANGE, OUTPUT);
     pinMode(carCountdownPin_RED, OUTPUT);
+    DEBUG_INFO("%15s: B: %s, D: %s", "start", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
 
-    PORTD = 0b00100111;  // Pedestrian: Display: off     (00), Light: Red     (10)
-    PORTB = 0b11000001;  // Car:        Display: off    (000), Light: Green  (001)
+    PORTD &= 0b00100111;  // Pedestrian: Display: off     (00), Light: Red     (10)
+    DEBUG_INFO("%15s: B: %s, D: %s", "PED: RED: off:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
+    PORTB &= 0b11000001;  // Car:        Display: off    (000), Light: Green  (001)
+    DEBUG_INFO("%15s: B: %s, D: %s", "CAR: GRN: off:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
 
     DEBUG_INFO("Ready");
     detectSignal(begSignalPin, 30);
@@ -86,24 +94,30 @@ void setup() {
 }
 
 void loop() {
-    PORTD = 0b10100111;  // Pedestrian: Display: RED     (10), Light: Red     (10)
-    PORTB = 0b11001001;  // Car:        Display: GREEN  (001), Light: Green  (001)
+    PORTD &= 0b10100111;  // Pedestrian: Display: RED     (10), Light: Red     (10)
+    DEBUG_INFO("%15s: B: %s, D: %s", "PED: RED: RED:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
+    PORTB &= 0b11001001;  // Car:        Display: GREEN  (001), Light: Green  (001)
+    DEBUG_INFO("%15s: B: %s, D: %s", "CAR: GRN: GRN:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
 
     delay(20 * 1000 + 50);
-    PORTB = 0b11010010;  // Car:        Display: ORANGE (010), Light: Orange (010)
-
+    PORTB &= 0b11010010;  // Car:        Display: ORANGE (010), Light: Orange (010)
+    DEBUG_INFO("%15s: B: %s, D: %s", "CAR: ORN: ORN:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
 
     delay(3 * 1000 + 50);
-    PORTB = 0b11100100;  // Car:        Display: RED    (100), Light: Red    (100)
+    PORTB &= 0b11100100;  // Car:        Display: RED    (100), Light: Red    (100)
+    DEBUG_INFO("%15s: B: %s, D: %s", "CAR: RED: RED:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
     
     delay(2 * 1000 + 50);
-    PORTD = 0b01010111;  // Pedestrian: Display: GREEN   (01), Light: Green   (01)
+    PORTD &= 0b01010111;  // Pedestrian: Display: GREEN   (01), Light: Green   (01)
+    DEBUG_INFO("%15s: B: %s, D: %s", "PED: GRN: GRN:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
 
     delay(15 * 1000 + 50);
-    PORTD = 0b00100111;  // Pedestrian: Display: off     (00), Light: Red     (10)
+    PORTD &= 0b00100111;  // Pedestrian: Display: off     (00), Light: Red     (10)
+    DEBUG_INFO("%15s: B: %s, D: %s", "PED: RED: off:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
 
     delay(2 * 1000 + 50);
-    PORTB = 0b11000001;  // Car:        Display: off    (000), Light: Green  (001)
+    PORTB &= 0b11000001;  // Car:        Display: off    (000), Light: Green  (001)
+    DEBUG_INFO("%15s: B: %s, D: %s", "CAR: GRN: off:", binaryToStr(PORTB, buf_b), binaryToStr(PORTD, buf_d));
     
     timeout = millis() + 60ul * 1000;
     DEBUG_INFO("Ready");
@@ -111,4 +125,14 @@ void loop() {
     DEBUG_INFO("%d", digitalRead(begSignalPin));
     while(!isDue(timeout)) {;}
     DEBUG_INFO("Start!");
+}
+
+char* binaryToStr(volatile uint8_t value, char* buffer) {
+    uint8_t tmp = value;
+    strncpy(buffer, "0x00000000", 11);
+    for(int i = 9; i >= 2; i--) {
+        buffer[i] = '0' + (0x00000001 & tmp);
+        tmp >>= 1;
+    }
+    return buffer;
 }
