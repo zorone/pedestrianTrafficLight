7beac5e9bc7999ac2dc5bfefa201e3bf2a7fdd0b
Imron jehleh <imron.jehleh@gmail.com>
2026-09-18 23:38:44 +0700

    Delay devices to wait for all devices become ready.

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,10 @@ void setup() {
     pinMode(carCountdownPin_ORANGE, OUTPUT);
     pinMode(carCountdownPin_RED, OUTPUT);
 
+    DEBUG_INFO("Wait for devices...");
+    delay(5 * 1000ul);   // Wait for all devices to become ready. 
+
+    DEBUG_INFO("Devices ready.");
     changeCarLightSignal(green);
     changePedestrianLightSignal(red);
     changeCarCountdownSignal(green, hide);
@@ -31,32 +35,42 @@ void setup() {
     DEBUG_INFO("Ready");
     detectSignal(begSignalPin, 30);
     DEBUG_INFO("%d", digitalRead(begSignalPin));
-    DEBUG_INFO("Start!");
 }
 
 void loop() {
+    DEBUG_INFO("Start!");
+    
     changeCarCountdownSignal(green, show);
     changePedestrianCountdownSignal(red, show);
+    DEBUG_INFO("Turn on Countdown");
+    DEBUG_INFO("Car: Green");
 
     delay(20 * 1000 + 50);
     changeCarLightSignal(orange);
     changeCarCountdownSignal(orange, show);
+    DEBUG_INFO("Car: Orange");
 
     delay(3 * 1000 + 50);
     changeCarLightSignal(red);
     changeCarCountdownSignal(red, show);
+    DEBUG_INFO("Car: Red");
 
     delay(2 * 1000 + 50);
     changePedestrianLightSignal(green);
     changePedestrianCountdownSignal(green, show);
+    DEBUG_INFO("Pedestrian: Green");
 
     delay(15 * 1000 + 50);
     changePedestrianLightSignal(red);
     changePedestrianCountdownSignal(red, hide);
+    DEBUG_INFO("Pedestrian: Red");
 
     delay(2 * 1000 + 50);
     changeCarLightSignal(green);
     changeCarCountdownSignal(green, hide);
+    DEBUG_INFO("Car: Green");
+    DEBUG_INFO("Turn off Countdown");
+    DEBUG_INFO("Finish Sequence");
 
     timeout = millis() + 60ul * 1000;
     DEBUG_INFO("Ready");
